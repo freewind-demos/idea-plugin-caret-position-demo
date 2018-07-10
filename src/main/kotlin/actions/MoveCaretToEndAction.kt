06828309894eea46470cd9e0da9b1c922da24e7f
freewind <nowindlee@gmail.com>
2018-07-11 00:08:07 +0800

    move caret demo

--- src/main/kotlin/actions/MoveCaretToEndAction.kt
+++ src/main/kotlin/actions/MoveCaretToEndAction.kt
@@ -2,19 +2,22 @@ package actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.project.impl.ProjectImpl
 import com.intellij.openapi.ui.Messages
 
-class HelloAction : AnAction() {
+class MoveCaretToEndAction : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
         (event.project as? ProjectImpl)?.let { project ->
             val editor = FileEditorManager.getInstance(project)?.selectedEditors?.filterIsInstance<TextEditor>()?.firstOrNull()
             if (editor != null) {
-                val offset = editor.editor.caretModel.offset
-                Messages.showInfoMessage("Caret position: $offset", "Caret Position")
+                val offset = editor.editor.document.textLength
+                ApplicationManager.getApplication().runWriteAction {
+                    editor.editor.caretModel.moveToOffset(offset)
+                }
             } else {
                 Messages.showWarningDialog("Please open a text file", "Can't work")
             }
---
+++ src/main/kotlin/actions/ShowCaretPositionAction.kt
@@ -0,0 +1,24 @@
+package actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.project.impl.ProjectImpl
+import com.intellij.openapi.ui.Messages
+
+class ShowCaretPositionAction : AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+        (event.project as? ProjectImpl)?.let { project ->
+            val editor = FileEditorManager.getInstance(project)?.selectedEditors?.filterIsInstance<TextEditor>()?.firstOrNull()
+            if (editor != null) {
+                val offset = editor.editor.caretModel.offset
+                Messages.showInfoMessage("Caret position: $offset", "Caret Position")
+            } else {
+                Messages.showWarningDialog("Please open a text file", "Can't work")
+            }
+        }
+    }
+
+}
