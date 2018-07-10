48a7b8c196d3118b8179ea13e3e14f13c8a3854f
freewind <nowindlee@gmail.com>
2018-07-10 22:49:13 +0800

    caret position demo

--- src/main/kotlin/actions/HelloAction.kt
+++ src/main/kotlin/actions/HelloAction.kt
@@ -2,12 +2,23 @@ package actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.project.impl.ProjectImpl
 import com.intellij.openapi.ui.Messages
 
 class HelloAction : AnAction() {
 
-    override fun actionPerformed(anActionEvent: AnActionEvent) {
-        Messages.showInfoMessage("Hello from my plugin", "Hello")
+    override fun actionPerformed(event: AnActionEvent) {
+        (event.project as? ProjectImpl)?.let { project ->
+            val editor = FileEditorManager.getInstance(project)?.selectedEditors?.mapNotNull { it as? TextEditor }?.firstOrNull()
+            if (editor != null) {
+                val offset = editor.editor.caretModel.offset
+                Messages.showInfoMessage("Caret position: $offset", "Caret Position")
+            } else {
+                Messages.showWarningDialog("Please open a text file", "Can't work")
+            }
+        }
     }
-    
+
 }
